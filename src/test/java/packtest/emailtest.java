1b77dd988c805d30060965bdca7c719e02174971
sindhura <sindhurayanamachintala123@gmail.com>
2023-10-07 15:02:55 +0530

    commit emailtest class again

--- src/test/java/packtest/emailtest.java
+++ src/test/java/packtest/emailtest.java
@@ -3,6 +3,7 @@ package packtest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -11,12 +12,14 @@ public class emailtest {
 
 	static WebDriver driver=null;
 	
+	@Parameters("username")
+
 	@Test
-	public static void emaillogin() throws InterruptedException {
+	public static void emaillogin(String uname) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.get("https://mail.google.com/");
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//input[@type='email']"));
+		driver.findElement(By.xpath("//input[@type='email']")).sendKeys(uname);
 	}
 }
